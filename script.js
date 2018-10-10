131c409f84bc047be7d6208ec7cae79db49b7c7b
ShuvoChisty <shuvochisti10@gmail.com>
2018-10-10 03:40:44 +0000

    Task1 & Task2

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 /* Task 1 */
 
-
+var heading=document.getElementById('heading');
+console.log(heading.innerText);
+/*heading.innerText="Changed it";*/
+/*heading.innerHTML=<h3>"Changed it"</h3>;*/
 /* Task 2 using plain JavaScript */
+var textBtn=document.getElementById('addText2');
+textBtn.addEventListener('click',function(event){
+   var task2=document.getElementById('task2a');
+   task2.innerText="Hello World";
+   var paragraph = document.createElement('p');
+   paragraph.innerText="Hello World";
+   task2.appendChild('paragraph');
+});
 
+function changeBGcolor(event){
+    var body=document.getElementsByTagName('body')[0];
+    if(event.target.innerText=="Red"){
+        body.style.backgroundColor='red';
+    }
+    if(event.target.innerText=="Green"){
+        body.style.backgroundColor='green';
+    }
+}
 
 
 /* Task 4 using jQuery */
